bbba8e2cc9ba723d9a8b54a86789326de1023746
Daniel Marin <damapa2003@gmail.com>
2024-11-29 20:26:11 +0100

    ADA Jueves 28

---
+++ src/main/kotlin/com/es/jwtSecurityKotlin/controller/SecretosExtraConfidencialesController.kt
@@ -0,0 +1,23 @@
+﻿package com.es.jwtSecurityKotlin.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/secretos_extra_confidenciales")
+class SecretosExtraConfidencialesController {
+
+    @PostMapping("/ficha1")
+    fun getFichaUno (): String {
+        return "Secreto de estado crack"
+    }
+
+    @GetMapping("/ficha2")
+    fun getFichaDos (): String {
+        return "No Secreto de estado crack"
+    }
+
+
+}
--- src/main/kotlin/com/es/jwtSecurityKotlin/service/UsuarioService.kt
+++ src/main/kotlin/com/es/jwtSecurityKotlin/service/UsuarioService.kt
@@ -3,10 +3,13 @@ package com.es.jwtSecurityKotlin.service
 import com.es.jwtSecurityKotlin.model.Usuario
 import com.es.jwtSecurityKotlin.repository.UsuarioRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class UsuarioService {
+class UsuarioService: UserDetailsService {
 
     @Autowired
     private lateinit var usuarioRepository: UsuarioRepository
@@ -15,6 +18,18 @@ class UsuarioService {
     /*
     TODO
      */
+
+    override fun loadUserByUsername(username: String?): UserDetails {
+       var usuario: Usuario = usuarioRepository
+           .findByUsername(username!!)
+           .orElseThrow()
+
+        return User.builder()
+            .username(usuario.username)
+            .password(usuario.password)
+            .roles(usuario.roles)
+            .build()
+    }
 
 
     /*
@@ -43,5 +58,6 @@ class UsuarioService {
         return null // Cambiar null por el usuario
 
     }
+
 
 }
